7b88982c80c8f7cdf50ab8df6565a760208e5854
dcapote92 <danicapote92@gmail.com>
2024-05-04 12:19:43 -0300

    test: find a person by id

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -8,18 +8,22 @@ import jakarta.persistence.Persistence;
 public class Program {
 
 	public static void main(String[] args) {
-		Person p1 = new Person(null, "Carlos da Silva","carlos@gmial.com");
-		Person p2 = new Person(null, "Joaquim Torres","joaquim@gmial.com");
-		Person p3 = new Person(null, "Ana Maria","ana@gmial.com");
+//		Person p1 = new Person(null, "Carlos da Silva","carlos@gmial.com");
+//		Person p2 = new Person(null, "Joaquim Torres","joaquim@gmial.com");
+//		Person p3 = new Person(null, "Ana Maria","ana@gmial.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+		
+		Person p = em.find(Person.class, 2);
+		System.out.println(p);
+		
 		System.out.println("Done!");
 		em.close();
 		emf.close();
